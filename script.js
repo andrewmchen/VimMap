605100b41a569fa3848e1496a0743c2f944d9575
andrewmchen <andrewmchen@berkeley.edu>
2014-01-04 19:46:52 -0800

    Working version. Needs work on ui and handleing some edge cases

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var input = ""
 var map = ""
-var defaults = {'i': 'insert', 'p': 'put', 'y': 'yank'}
+var defaults = {'i': 'VIM DEFAULT: insert', 'p': 'VIM DEFAULT: put', 'y': 'VIM DEFAULT: yank'}
+var conversion = {'\\': 'leader', ',': 'comma', '.':'period', ';' :'semicolon'}
 var custom = {}
 $(document).ready(function(){
     console.log("hi");
@@ -13,6 +14,7 @@ $(document).ready(function(){
         parseCustomMappings();
     });
     $(document).keypress(function(event){
+        $("#title").html("");
         console.log(event);
         $("#keystroke").append(String.fromCharCode(event['charCode'])+" ");
         input+=String.fromCharCode(event['charCode'])
@@ -24,6 +26,7 @@ $(document).ready(function(){
             $("#keystroke").html("&nbsp");
             input="";
         }
+        /*
         if(event['keyCode']==17){
             $("#keystroke").append("Ctrl ");
             input+="ç";
@@ -32,6 +35,7 @@ $(document).ready(function(){
             $("#keystroke").append("Shift ");
             input+="ß";
         }
+        */
         render();
     });
 });
@@ -46,25 +50,72 @@ function render(){
     for(element in defaults){
         if(element==input){
             $("#description").html(defaults[element]);
+            done();
         }
-        if(!S(element).startsWith(input)) break;
         var chomped = S(element).chompLeft(input);
-        if (S(chomped).length==1){
-            console.log(chomped+" is turning red");
-            $("#"+chomped).css('background', 'red');
+        if (S(chomped).length==1 && S(element).startsWith(input)){
+            $("#"+chomped).css('background', '#ff99e1');
         }
     }
+
     // render customs
+    for(element in custom){
+        console.log(element);
+        if(element==input){
+            $("#description").html(custom[element]);
+            $('li').css('background','#EBDDCC');
+        }
+        // only do something if it starts with the input
+        if(S(element).startsWith(input)){
+            var chomped = S(element).chompLeft(input).s;
+            //if the chomped string has length 1 then we do something
+            console.log('chomped is '+ chomped);
+            //if length 1
+            if(chomped.length==1){
+
+                if(isUpper(chomped)){
+                    $("#"+chomped.toLowerCase()).css('color', '#9C6011');
+                    $("#"+chomped.toLowerCase()).css('font-weight', 'bolder');
+                    $("#"+chomped.toLowerCase()).css('font-size', '150%');
+                    $("#"+chomped.toLowerCase()).text($("#"+chomped.toLowerCase()).text().toUpperCase());
+                }
+                else{
+                console.log('here');
+                    if(isConversion(chomped)){
+                        $("#"+conversion[chomped]).css('background', '#e95a22');
+                    }
+                    else $("#"+chomped).css('background', '#e95a22');
+                }
+            }
+            else {
+                console.log(chomped.substring(0,1));
+                if(conversion.hasOwnProperty(chomped.substring(0,1))) $("#"+conversion[chomped.substring(0,1)]).css('background','#c0e7d9');
+                else $("#"+chomped.substring(0,1)).css('background','#c0e7d9');
+            }
+        }
+    }
+}
+function isConversion(c){
+    return conversion.hasOwnProperty(c);
+}
+function isUpper(c){
+    var x = c.charCodeAt(0);
+    if((x>64&&x<91)||(x>96&&x<123)||(x>127&&x<155)||(x>159&&x<166)) return (c==c.toUpperCase());
+    else return false;
 }
 
 //resets colors and description
 function reset(){
     $("#description").html("&nbsp");
-    $(".function").css('background', '#fff');
-    $(".letter").css('background', '#fff');
-    $(".symbol").css('background', '#fff');
+
+    $('li').attr('style', '');
+
 }
 
+//done
+function done(){
+    $('li').css('background','#ffcfcc');
+}
 //Create custom object of mappings
 //NEED TO ADD ERROR HANDLING FOR " in mappings
 function parseCustomMappings(){
@@ -91,5 +142,6 @@ function parseCustomMappings(){
     customString += "}"
     console.log(customString);
     eval("custom =" +customString);
+    render();
 }
 
